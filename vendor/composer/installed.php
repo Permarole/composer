9d03e78e845264ce7f8d65258f5dad040c1a52a3
Jonathan PERMAROLE <jonathan.permarole@hotmail.com>
2021-10-16 15:48:03 +0200

    init files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'john/project-architecture',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'john/project-architecture' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
